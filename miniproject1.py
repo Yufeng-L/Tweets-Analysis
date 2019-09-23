abde449cc4b850721de789fab32f17c18e1475f3
YufengL <yflin@bu.edu>
2019-09-22 23:53:08 -0400

    sprint2

--- miniproject1.py
+++ miniproject1.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 consumer_key = ""
 consumer_secret = ""
@@ -16,3 +17,34 @@ try:
     print("Authentication OK")   #verify authentication
 except:
     print("Error during authentication")
+
+
+ #get sepecifc info such as food or any interests
+
+ search_info = api.search(q='starbucks',count = 100)    #starbucks related in this case
+
+ for tweet in search_info:
+
+ 	if 'text' in tweet._json:
+ 		print(tweet._json['text'])   #print contents
+
+ 		output_file.write(json.dumps(tweet._json))  
+
+
+ #get user timeline, such as starbucks
+
+ for alltweets in tweepy.Cursor(api.user.timeline,screen_name ='@Starbucks Coffee',tweet_mode = "extended").items():
+
+ 	print(alltweets.full_text)
+
+ 
+
+
+
+
+
+
+
+
+
+
