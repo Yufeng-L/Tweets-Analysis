99492f127bb5a5ad122b1d4abde4949ec3e5ba77
YufengL <yflin@bu.edu>
2019-09-23 16:03:11 -0400

    sprint2

--- miniproject1.py
+++ miniproject1.py
@@ -1,10 +1,10 @@
 import tweepy
 import json
 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "nlWtCtKvRrWUCMMRvM4za8Ab2"
+consumer_secret = "08gBDUtkr07FIzCvNM8IsAlXkmPADS6TAoXfUjpOkyKGxF06FQ"
+access_token = "1172954596890697728-DC5o6nXgajbZ1W3Bng9UTSFPsWP2Q6"
+access_token_secret = "SjEelcwH3OzRgUpL1nELv9e0t9Z6VvIsTo3Iyar0PwGeK"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -18,24 +18,40 @@ try:
 except:
     print("Error during authentication")
 
-
+searchlist = []; #hold search list
  #get sepecifc info such as food or any interests
+search_info = api.search(q='starbucks',count = 100)    #starbucks related in this case
 
- search_info = api.search(q='starbucks',count = 100)    #starbucks related in this case
+searchlist.extend(search_info);
 
- for tweet in search_info:
-
- 	if 'text' in tweet._json:
+for tweet in search_info:
+	if 'text' in tweet._json:
  		print(tweet._json['text'])   #print contents
 
- 		output_file.write(json.dumps(tweet._json))  
+#write objects to json
+file = open('tweet.json', 'w') 
+print("Writing tweet objects to JSON file")
+for status in searchlist:
+
+    json.dump(status._json,file,sort_keys = True,indent = 4)
+
+    
+    #close the file
+print("Done")
+file.close()
 
 
- #get user timeline, such as starbucks
 
- for alltweets in tweepy.Cursor(api.user.timeline,screen_name ='@Starbucks Coffee',tweet_mode = "extended").items():
 
- 	print(alltweets.full_text)
+print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ')
+#get user timeline, such as starbucks, for latest 100 tweets
+
+tweets = api.user_timeline(id='Starbucks',count=100,tweet_mode="extended")
+
+for t in tweets:
+
+    print(t.full_text)
+	
 
  
 
